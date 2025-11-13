5b885f9303e03f876529694def1cbebb067f77c9
Shivam Dwivedi <shivamdwivedicse20919@gmail.com>
2025-11-13 19:04:06 +0530

    Add files via upload
    Write and create a file in python using file handling

---
+++ file_handle.ex1.py
@@ -0,0 +1,7 @@
+# create and write a file :-
+file = open('example1', "w")
+file.write("hello i am learning file handling in python \n")
+file.write("Line seconed.")
+file.close()
+
+print("File created and writeen successfully!!")
